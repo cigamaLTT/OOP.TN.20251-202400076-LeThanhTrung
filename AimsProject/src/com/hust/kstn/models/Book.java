d34098536406feeef99e631f62480cb45b237f4a
cigamaLTT <trunglcct@gmail.com>
2025-11-16 22:11:25 +0700

    create book class and book test

---
+++ AimsProject/src/com/hust/kstn/models/Book.java
@@ -0,0 +1,112 @@
+package com.hust.kstn.models;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Book {
+    public static class BookAuthor{
+        private String fullName;
+        private int yearOfBirth;
+        private String shortBiography;
+
+        public String getFullName() {
+            return fullName;
+        }
+
+        public void setFullName(String fullName) {
+            this.fullName = fullName;
+        }
+
+        public int getYearOfBirth() {
+            return yearOfBirth;
+        }
+
+        public void setYearOfBirth(int yearOfBirth) {
+            this.yearOfBirth = yearOfBirth;
+        }
+
+        public String getShortBiography() {
+            return shortBiography;
+        }
+
+        public void setShortBiography(String shortBiography) {
+            this.shortBiography = shortBiography;
+        }
+
+        public BookAuthor(String fullName, int yearOfBirth, String shortBiography) {
+            this.fullName = fullName;
+            this.yearOfBirth = yearOfBirth;
+            this.shortBiography = shortBiography;
+        }
+
+        @Override
+        public String toString() {
+            return this.fullName + " ("
+                    + this.yearOfBirth + ", "
+                    + this.shortBiography + ")\n";
+        }
+
+    }
+
+
+    private List<BookAuthor> authors = new ArrayList<>();
+
+    private int id;
+    private String title;
+    private String category;
+    private double cost;
+    private int numOfTokens;
+
+    public List<BookAuthor> getAuthors() {
+        return authors;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public double getCost() {
+        return cost;
+    }
+
+    public int getNumOfTokens() {
+        return numOfTokens;
+    }
+
+    public Book(int id, String title, String category, double cost, int numOfTokens, BookAuthor... authors) {
+        this.authors.addAll(Arrays.asList(authors));
+        this.id = id;
+        this.title = title;
+        this.category = category;
+        this.cost = cost;
+        this.numOfTokens = numOfTokens;
+    }
+
+    public Book(int id, String title, String category, double cost, int numOfTokens, List<BookAuthor> authors) {
+        this.id = id;
+        this.title = title;
+        this.category = category;
+        this.cost = cost;
+        this.numOfTokens = numOfTokens;
+        this.authors = authors;
+    }
+
+    @Override
+    public String toString() {
+        return "Book[" + this.id + "]["
+                + this.title + "]["
+                + this.cost + "]["
+                + this.category + "]["
+                + this.numOfTokens + "]"
+                + "\nAuthors: " + this.authors;
+    }
+}
---
+++ AimsProject/src/com/hust/kstn/test/BookTest.java
@@ -0,0 +1,60 @@
+package com.hust.kstn.test;
+
+import com.hust.kstn.models.Book;
+import com.hust.kstn.models.Book.BookAuthor;
+import java.util.ArrayList;
+import java.util.List;
+
+public class BookTest {
+
+    public static void main(String[] args) {
+
+        BookAuthor author1 = new BookAuthor("Author One", 1980, "Biography of Author One.");
+        BookAuthor author2 = new BookAuthor("Author Two", 1992, "Biography of Author Two.");
+
+
+        Book book1 = new Book(
+                101,
+                "The First Book",
+                "Fiction",
+                25.50,
+                50000,
+                author1
+        );
+
+        System.out.println("--- TESTING BOOK 1 (VARARGS CONSTRUCTOR) ---");
+        System.out.println(book1);
+        System.out.println();
+
+
+        List<BookAuthor> authorsForBook2 = new ArrayList<>();
+        authorsForBook2.add(author1);
+        authorsForBook2.add(author2);
+
+        Book book2 = new Book(
+                102,
+                "The Second Book",
+                "Non-Fiction",
+                35.75,
+                75000,
+                authorsForBook2
+        );
+
+        System.out.println("--- TESTING BOOK 2 (LIST CONSTRUCTOR) ---");
+        System.out.println(book2);
+        System.out.println();
+
+
+        Book book3 = new Book(
+                103,
+                "A Book with Two Authors",
+                "Science",
+                40.00,
+                120000,
+                author1, author2
+        );
+
+        System.out.println("--- TESTING BOOK 3 (VARARGS WITH 2 AUTHORS) ---");
+        System.out.println(book3);
+    }
+}
