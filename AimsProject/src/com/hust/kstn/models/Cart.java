699ee92f619a36d114cc7718b1f7ca38d3e35e69
cigamaLTT <trunglcct@gmail.com>
2025-11-16 19:04:08 +0700

    Complete section 2.1: Method Overloading

--- AimsProject/src/com/hust/kstn/models/Cart.java
+++ AimsProject/src/com/hust/kstn/models/Cart.java
@@ -5,14 +5,57 @@ public class Cart {
     private DigitalVideoDisc itemsInCart[] = new DigitalVideoDisc[MAX_NUMBER_ORDERED];
     private int qtyOrdered = 0;
 
-    public void add_DVD(DigitalVideoDisc disc){
+    public boolean addOneDVD(DigitalVideoDisc disc){
         if (qtyOrdered >= MAX_NUMBER_ORDERED){
-            System.out.println("The cart is full");
-            return;
+            return false;
         }
         this.itemsInCart[this.qtyOrdered] = disc;
         this.qtyOrdered++;
-        System.out.println("The disc has been added successfully");
+        return true;
+    }
+
+    public void add_DVD(DigitalVideoDisc disc){
+        if(addOneDVD(disc)) {
+            System.out.println("The disc has been added successfully");
+        }else{
+            System.out.println("The Cart is full");
+        }
+    }
+
+    public void addDVD(DigitalVideoDisc disc1, DigitalVideoDisc disc2) {
+        boolean isDisc1Added = addOneDVD(disc1);
+        boolean isDisc2Added = addOneDVD(disc2);
+
+        if(isDisc1Added && isDisc2Added){
+            System.out.println("Both discs has been added successfully");
+        } else if(isDisc1Added && !isDisc2Added){
+            System.out.println("Only " + disc1.getTitle() + " was added, the Cart is full");
+        } else{
+            System.out.println("The Cart is full, both discs were rejected");
+        }
+    }
+
+    public void addDVD(DigitalVideoDisc[] discs) {
+        boolean isAtLeastOneDiskAdded = false;
+        boolean isAtLeastOneDiskRejected = false;
+        for (DigitalVideoDisc disc : discs){
+            if(addOneDVD(disc)){
+                if(!isAtLeastOneDiskAdded){
+                    System.out.print("List of Discs have been added: [" + disc.getTitle());
+                }else {
+                    System.out.print(", " + disc.getTitle());
+                }
+                isAtLeastOneDiskAdded = true;
+            } else{
+                isAtLeastOneDiskRejected = true;
+                break;
+            }
+        }
+        if(isAtLeastOneDiskRejected){
+            System.out.println("]\nThe Cart is full, cannot add other discs");
+        } else{
+            System.out.println("]\nAll discs have been added successfully");
+        }
     }
 
     public void remove_DVD(DigitalVideoDisc disc){
