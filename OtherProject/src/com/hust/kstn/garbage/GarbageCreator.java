e6b2309ee9867e002fa21a5581c7d3b5c0690655
cigamaLTT <trunglcct@gmail.com>
2025-11-17 09:52:44 +0700

    commited

---
+++ OtherProject/src/com/hust/kstn/garbage/GarbageCreator.java
@@ -0,0 +1,30 @@
+package com.hust.kstn.garbage;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class GarbageCreator {
+    public static void main(String[] args) {
+        String filename = "OtherProject/test.exe";
+
+        byte[] inputBytes = {0};
+        long startTime, endTime;
+
+        try {
+            inputBytes = Files.readAllBytes(Paths.get(filename));
+
+            startTime = System.currentTimeMillis();
+            String outputString = "";
+            for (byte b : inputBytes) {
+                outputString += (char) b;
+            }
+            endTime = System.currentTimeMillis();
+
+            System.out.println("GarbageCreator (String +) Time: " + (endTime - startTime) + "ms");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
